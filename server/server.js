7cd1b0d9c1bc728e706d39e8ffddd3e15766c3d6
Abhishek <shek54112@gmail.com>
2025-06-18 18:36:12 +0530

    Fix 404 on Vercel with vercel.json

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ import cookieParser from "cookie-parser";
 const app = express();
 const port = 9000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://rk-flex-freelance-work.vercel.app", credentials: true }));
+
+
+
+
 app.use(cookieParser());
 app.use(express.json());
 
