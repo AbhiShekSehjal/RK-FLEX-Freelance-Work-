a4029e69459418ac3fff138b43f9bbde2d43b418
Abhishek <shek54112@gmail.com>
2025-08-15 21:08:51 +0530

    15-05-25 9th commit

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -7,7 +7,7 @@ import toast from "react-hot-toast";
 import { axiosInstance } from "../../lib/axios.js";
 import { useOrders } from "../../store/useOrders.js";
 
-function Cart({ className, onClose, refreshTrigger }) {
+function Cart({ className, onClose, refreshTrigger,handleRefresh }) {
 
     const { productCard, cartItems, setCartItems } = useWallsStore();
     const { handler } = useOrders();
@@ -115,7 +115,7 @@ function Cart({ className, onClose, refreshTrigger }) {
                     &times;
                 </div>
 
-                {/* <button className="bookmarkBtn" onClick={handleRefresh}>
+                <button className="bookmarkBtn" onClick={handleRefresh}>
                     <span className="IconContainer">
                         <div className="svg-wrapper">
                             <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.793 2.293a1 1 0 0 1 1.414 0l3 3a1 1 0 0 1 0 1.414l-3 3a1 1 0 0 1-1.414-1.414L14.086 7H12.5C8.952 7 6 9.952 6 13.5S8.952 20 12.5 20s6.5-2.952 6.5-6.5a1 1 0 1 1 2 0c0 4.652-3.848 8.5-8.5 8.5S4 18.152 4 13.5 7.848 5 12.5 5h1.586l-1.293-1.293a1 1 0 0 1 0-1.414z" fill="#0D0D0D" /></svg>
@@ -125,7 +125,7 @@ function Cart({ className, onClose, refreshTrigger }) {
                 </button>
                 {cartItems.length !== 0 &&
                     <p>Refreah to See new added product</p>
-                } */}
+                }
             </div>
 
             <div className="selectedProducts">
